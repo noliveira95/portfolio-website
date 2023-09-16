0cccb511ae5ae9c43dfa2e80367a9cbccc65251d
noliveira95 <noliveira95@outlook.com>
2023-09-15 20:16:07 -0700

    refactor: add basic nav structure

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./navbar.module.css";
+import Image from "next/image";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className="navbar">
+        <Image className="logo" src="" alt="logo" width={128} height={77} />
+        <div className="navLinks"></div>
+        <button className="btn">
+          <Image className="logo" src="" alt="logo" />
+          Download CV
+        </button>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
