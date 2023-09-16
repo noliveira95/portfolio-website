5f6ce407040a0a2b3920a9f5bee0824c40f507ee
noliveira95 <noliveira95@outlook.com>
2023-09-16 15:13:50 -0700

    feat: add links to navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./navbar.module.css";
 import Image from "next/image";
+import Link from "next/link";
 import { DownloadIcon } from "@radix-ui/react-icons";
 
 function Navbar() {
@@ -8,7 +9,12 @@ function Navbar() {
     <>
       <nav className="navbar">
         <Image className="logo" src="" alt="logo" width={128} height={77} />
-        <div className="navLinks"></div>
+        <div className="navLinks">
+          <Link href="/">Home</Link>
+          <Link href="/">Who Am I</Link>
+          <Link href="/">My Skills</Link>
+          <Link href="/">My Work</Link>
+        </div>
         <button className="btn">
           <DownloadIcon />
           Download CV
