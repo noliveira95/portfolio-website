8b5ce269cb49886fad435757c30b2a9cb5a22725
noliveira95 <noliveira95@outlook.com>
2023-09-17 12:17:53 -0700

    refactor: wrap Button with Link

--- components/shared/Button/Button.jsx
+++ components/shared/Button/Button.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "./Button.module.css";
 import { DownloadIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 function Button() {
   return (
     <>
-      <button className={styles["btn"]}>
-        <DownloadIcon className={styles["btn-icon"]} color="white" />
-        Download CV
-      </button>
+      <Link type="button" href="/">
+        <button className={styles["btn"]}>
+          <DownloadIcon className={styles["btn-icon"]} color="white" />
+          Download CV
+        </button>
+      </Link>
     </>
   );
 }
