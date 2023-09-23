8e685650085281f254118d87894cdc073df7eac2
noliveira95 <noliveira95@outlook.com>
2023-09-23 14:00:43 -0700

    style: add styles for About, update Hero styles

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./About.module.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <section id={styles.about}>
+      <div className={styles["about-content"]}>
+        <h2>Who Am I?</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+          deserunt ut sed explicabo perspiciatis minima similique non qui veniam
+          repellendus molestiae esse nobis voluptatibus debitis deleniti ipsam
+          id, perferendis vel!
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
