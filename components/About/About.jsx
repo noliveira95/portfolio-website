eb58dd2bc6876583301b0dfdff84ab289ac40048
noliveira95 <noliveira95@outlook.com>
2023-09-23 14:11:28 -0700

    feat: add image to About

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import styles from "./About.module.css";
+import Image from "next/image";
 
 function About() {
   return (
     <section id={styles.about}>
       <div className={styles["about-content"]}>
         <h2>Who Am I?</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-          deserunt ut sed explicabo perspiciatis minima similique non qui veniam
-          repellendus molestiae esse nobis voluptatibus debitis deleniti ipsam
-          id, perferendis vel!
-        </p>
+        <div className="about-grid">
+          <Image
+            src="/MyPic.png"
+            alt="Picture of the author"
+            width={240}
+            height={240}
+          />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            deserunt ut sed explicabo perspiciatis minima similique non qui
+            veniam repellendus molestiae esse nobis voluptatibus debitis
+            deleniti ipsam id, perferendis vel!
+          </p>
+        </div>
       </div>
     </section>
   );
