13f259a57e102c6f5d511c83d69bdd360a2e10f7
noliveira95 <noliveira95@outlook.com>
2023-09-23 14:24:04 -0700

    style: add flex display to About content

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -5,22 +5,20 @@ import Image from "next/image";
 function About() {
   return (
     <section id={styles.about}>
+      <h2>Who Am I?</h2>
       <div className={styles["about-content"]}>
-        <h2>Who Am I?</h2>
-        <div className="about-grid">
-          <Image
-            src="/MyPic.png"
-            alt="Picture of Nicholas"
-            width={240}
-            height={240}
-          />
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-            deserunt ut sed explicabo perspiciatis minima similique non qui
-            veniam repellendus molestiae esse nobis voluptatibus debitis
-            deleniti ipsam id, perferendis vel!
-          </p>
-        </div>
+        <Image
+          src="/MyPic.png"
+          alt="Picture of Nicholas"
+          width={240}
+          height={240}
+        />
+        <p className={styles["about-paragraph"]}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+          deserunt ut sed explicabo perspiciatis minima similique non qui veniam
+          repellendus molestiae esse nobis voluptatibus debitis deleniti ipsam
+          id, perferendis vel!
+        </p>
       </div>
     </section>
   );
