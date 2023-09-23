4110feda5c2122db51d4120f5ced93785e872de6
noliveira95 <noliveira95@outlook.com>
2023-09-23 13:45:19 -0700

    feat: start About section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About/About";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 }
