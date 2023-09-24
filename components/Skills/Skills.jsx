c77ad93c33d85d267928411a5c9d002d89292196
noliveira95 <noliveira95@outlook.com>
2023-09-24 16:37:14 -0700

    feat: start SkillCard component

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./Skills.module.css";
+import SkillCard from "./SkillCard";
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div id={styles.skills}>
+      <h2>My Skills</h2>
+      <div className={styles["skills-container"]}>
+        <SkillCard />
+        <SkillCard />
+        <SkillCard />
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
