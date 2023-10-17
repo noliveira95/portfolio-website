173c5a369edf4c61208f2fc29490b397970408a3
noliveira95 <noliveira95@outlook.com>
2023-10-17 15:22:29 -0700

    refactor: rebuild Portfolio and Projects, replace JSON object with array

--- components/Portfolio/Portfolio.jsx
+++ components/Portfolio/Portfolio.jsx
@@ -1,30 +1,16 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./Portfolio.module.css";
 import Projects from "./Projects/Projects";
 import data from "./Projects/projects.json";
-import validate from "./Projects/validate";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
 function Portfolio() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [validatedData, setValidatedData] = useState(null);
-
-  async function validateData() {
-    try {
-      const valid = await validate(data);
-      setValidatedData(valid);
-    } catch {
-      console.log("error");
-    }
-  }
-
-  useEffect(() => {
-    validateData();
-  }, [validatedData]);
 
   const handleNext = () => {
-    if (currentIndex === data.projects.length - 1) {
+    if (currentIndex === data.length - 1) {
       setCurrentIndex(0);
     } else {
       setCurrentIndex(currentIndex + 1);
@@ -34,7 +20,7 @@ function Portfolio() {
 
   const handlePrevious = () => {
     if (currentIndex < 0) {
-      setCurrentIndex(data.projects.length - 1);
+      setCurrentIndex(data.length - 1);
     } else {
       setCurrentIndex(currentIndex - 1);
     }
@@ -46,16 +32,41 @@ function Portfolio() {
   };
 
   return (
-    <section id={styles.portfolio}>
-      <h2>Portfolio</h2>
-      <Projects
-        currentIndex={currentIndex}
-        handleNext={handleNext}
-        handlePrevious={handlePrevious}
-        handleDotClick={handleDotClick}
-        data={data}
-      />
-    </section>
+    <>
+      <section id={styles.portfolio}>
+        <h2>Portfolio</h2>
+
+        <div className={styles["project-carousel"]}>
+          <div className={styles["project-with-btns"]}>
+            <div className={styles.left} onClick={handlePrevious}>
+              <ChevronLeftIcon width={50} height={80} />
+            </div>
+            <div className={styles["project-container"]}>
+              <Projects
+                currentIndex={currentIndex}
+                title={data[currentIndex].title}
+                description={data[currentIndex].description}
+                image={data[currentIndex].featured_image}
+              />
+            </div>
+            <div className={styles.right} onClick={handleNext}>
+              <ChevronRightIcon width={50} height={80} />
+            </div>
+          </div>
+          <div className={styles.indicator}>
+            {data.map((_, index) => (
+              <div
+                key={index}
+                className={`${styles.dot} ${
+                  index === currentIndex ? styles.active : ""
+                }`}
+                onClick={() => handleDotClick(index)}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
--- components/Portfolio/Projects/Projects.jsx
+++ components/Portfolio/Projects/Projects.jsx
@@ -4,53 +4,28 @@ import React from "react";
 import styles from "../Portfolio.module.css";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
-import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
-function Projects({
-  currentIndex,
-  handleNext,
-  handlePrevious,
-  handleDotClick,
-  data,
-}) {
+function Projects({ currentIndex, title, description, image }) {
   return (
-    <div className={styles["project-carousel"]}>
-      <div className={styles["project-image-with-btns"]}>
-        <div className={styles.left} onClick={handlePrevious}>
-          <ChevronLeftIcon width={50} height={80} />
-        </div>
+    <>
+      <div className={styles["project-items"]}>
         <Image
           className={styles["project-image"]}
           key={currentIndex}
-          src={data.projects[currentIndex].featured_image}
+          src={image}
           alt="Project"
           width={600}
           height={400}
         ></Image>
-        <div className={styles.right} onClick={handleNext}>
-          <ChevronRightIcon width={50} height={80} />
+
+        <div className={styles["project-title"]}>
+          <h2>{title}</h2>
+        </div>
+        <div className={styles["project-description"]}>
+          <p>{description}</p>
         </div>
       </div>
-
-      <div className={styles["project-title"]}>
-        <h2>{data.projects[currentIndex].title}</h2>
-      </div>
-      <div className={styles["project-description"]}>
-        <p>{data.projects[currentIndex].description}</p>
-      </div>
-
-      <div className={styles.indicator}>
-        {data.projects.map((_, index) => (
-          <div
-            key={index}
-            className={`${styles.dot} ${
-              index === currentIndex ? styles.active : ""
-            }`}
-            onClick={() => handleDotClick(index)}
-          ></div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
 
