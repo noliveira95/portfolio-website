6062c722267e6a721ce8fe77b2acdb19d1da5226
noliveira95 <noliveira95@outlook.com>
2023-10-22 16:32:38 -0700

    style: adjust logo size and spacing

--- components/shared/Card/Card.jsx
+++ components/shared/Card/Card.jsx
@@ -6,13 +6,15 @@ import Button from "../Button/Button";
 function CustomCard({ cardImage, cardTitle, cardText, cardCTA, cardCTAText }) {
   return (
     <div className={styles["card"]}>
-      <Image
-        src={cardImage}
-        alt="skill"
-        width={200}
-        height={140}
-        className={styles["card-image"]}
-      />
+      <div className={styles["card-image-wrapper"]}>
+        <Image
+          src={cardImage}
+          alt="skill"
+          width={160}
+          height={140}
+          className={styles["card-image"]}
+        />
+      </div>
       <div className={styles["card-content"]}>
         <h3 className={styles["card-title"]}>{cardTitle}</h3>
         <p className={styles["card-description"]}>{cardText}</p>
