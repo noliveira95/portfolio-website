6a2a24de8163759ae04044219571fd5ccf14306c
noliveira95 <noliveira95@outlook.com>
2023-10-28 16:01:29 -0700

    feat: add form component, style form

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Contact.module.css";
+import ContactForm from "./ContactForm";
 
 function Contact() {
   return (
     <section id={styles.contact}>
       <h2>Contact Me</h2>
+      <div className={styles["contact-form-wrapper"]}>
+        <ContactForm />
+      </div>
     </section>
   );
 }
--- components/Contact/ContactForm.jsx
+++ components/Contact/ContactForm.jsx
@@ -1,8 +1,49 @@
+"use client";
+
 import React from "react";
 import { useForm } from "react-hook-form";
+import styles from "./Contact.module.css";
 
 function ContactForm() {
-  return <div>ContactForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form className={styles["contact-form"]} onSubmit={handleSubmit(onSubmit)}>
+      <input
+        className={styles["contact-form-field"]}
+        type="text"
+        placeholder="First name"
+        {...register("First name", { required: true, maxLength: 80 })}
+      />
+      <input
+        className={styles["contact-form-field"]}
+        type="text"
+        placeholder="Last name"
+        {...register("Last name", { required: true, maxLength: 100 })}
+      />
+      <input
+        className={styles["contact-form-field"]}
+        type="text"
+        placeholder="Email"
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <textarea
+        className={styles["contact-form-field"]}
+        {...register("Additional Information", {
+          required: false,
+          maxLength: 250,
+        })}
+      />
+
+      <input className={styles.submit} type="submit" />
+    </form>
+  );
 }
 
 export default ContactForm;
