ea1ae4eeb51df4f1331b9db420fc5794f9d8785f
noliveira95 <noliveira95@outlook.com>
2023-10-27 19:49:19 -0700

    refactor: remove comments

--- components/Portfolio/Projects/Projects.jsx
+++ components/Portfolio/Projects/Projects.jsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import styles from "../Portfolio.module.css";
-import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
 import CustomCard from "@/components/shared/Card/Card";
 
@@ -10,36 +9,6 @@ function Projects({ currentIndex, data, handleDotClick }) {
   return (
     <>
       <div className={styles["project-items"]}>
-        {/* <Image
-          className={styles["project-image"]}
-          key={currentIndex}
-          src={
-            data.length > 0
-              ? data[currentIndex].featured_image
-              : "/osman-talha-dikyar-NMCABEhN0RE-unsplash.jpg"
-          }
-          alt="Project"
-          width={700}
-          height={400}
-        ></Image> */}
-        {/* <div className={styles["client-logo-wrapper"]}>
-          <Image
-            className={styles["client-logo"]}
-            src={
-              data.length > 0
-                ? data[currentIndex].client_logo
-                : "/osman-talha-dikyar-NMCABEhN0RE-unsplash.jpg"
-            }
-            alt="Client Logo"
-            width={200}
-            height={200}
-          ></Image>
-        </div>
-        <div className={styles["project-description"]}>
-          <p>
-            {data.length > 0 ? data[currentIndex].description : "Description"}
-          </p>
-        </div> */}
         <CustomCard
           cardImage={
             data.length > 0
@@ -64,10 +33,6 @@ function Projects({ currentIndex, data, handleDotClick }) {
             ></div>
           ))}
         </div>
-
-        {/* <div className={styles["project-title"]}>
-          <h2>{data.length > 0 ? data[currentIndex].title : "Title"}</h2>
-        </div> */}
       </div>
     </>
   );
