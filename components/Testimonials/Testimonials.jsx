5c2b8fa6c8478bb22454f528562aea2268aac94f
noliveira95 <noliveira95@outlook.com>
2023-10-28 14:48:27 -0700

    style: change testimonials text style, add heading

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -4,7 +4,7 @@ import styles from "./Testimonials.module.css";
 function Testimonials() {
   return (
     <section id={styles.testimonials}>
-      {/* <h2 className={styles["testimonials-heading"]}>Testimonials</h2> */}
+      <h2 className={styles["testimonials-heading"]}>Testimonials</h2>
       <div className={styles["testimonial-container"]}>
         <blockquote className={styles["testimonial-blockquote"]}>
           <p className={styles["testimonial-quote"]}>
@@ -12,9 +12,14 @@ function Testimonials() {
             right questions and quickly delivered results. I would 100% work
             with him again!&quot;
           </p>
-          <cite className={styles["testimonial-author"]}>
-            - Joshua Leavitt, Chief Executive Officer (CEO)
-          </cite>
+          <div className="testimonial-author-wrapper">
+            <cite className={styles["testimonial-author"]}>
+              Joshua Leavitt{" "}
+            </cite>
+          </div>
+          <div className={styles["author-title-wrapper"]}>
+            <cite className={styles["author-title"]}>CEO, Intagly</cite>
+          </div>
         </blockquote>
       </div>
     </section>
