188a7c9e13a67792d4afa4d79d7ec143d506802b
noliveira95 <noliveira95@outlook.com>
2023-10-29 13:05:01 -0700

    refactor: connect emailjs to form

--- components/Contact/ContactForm.jsx
+++ components/Contact/ContactForm.jsx
@@ -1,49 +1,93 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 import styles from "./Contact.module.css";
 
 function ContactForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  // };
+
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      user_first_name: "",
+      user_last_name: "",
+      user_email: "",
+      message: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    const templateParams = {
+      user_name: data.user_first_name + " " + data.user_last_name,
+      user_email: data.user_email,
+      message: data.message,
+    };
+
+    emailjs
+      .sendForm(
+        "contact_service",
+        "contact_form",
+        templateParams,
+        "Igz98Ny1j25t2gvsI"
+      )
+      .then(
+        (result) => {
+          console.log("SUCCESS!");
+          console.log(result.text);
+          reset();
+        },
+        (error) => {
+          console.log("FAILED...");
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
-    <form className={styles["contact-form"]} onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className={styles["contact-form"]}
+      onSubmit={handleSubmit(onSubmit)}
+      ref={form}
+    >
       <input
         className={styles["contact-form-field"]}
         type="text"
         placeholder="First name"
-        {...register("First name", { required: true, maxLength: 80 })}
+        name="user_first_name"
+        {...register("user_first_name", { required: true, maxLength: 80 })}
       />
       <input
         className={styles["contact-form-field"]}
         type="text"
         placeholder="Last name"
-        {...register("Last name", { required: true, maxLength: 100 })}
+        name="user_last_name"
+        {...register("user_last_name", { required: true, maxLength: 100 })}
       />
       <input
         className={styles["contact-form-field"]}
         type="text"
         placeholder="Email"
-        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+        name="user_email"
+        {...register("user_email", { required: true, pattern: /^\S+@\S+$/i })}
       />
       <textarea
         className={styles["contact-form-field"]}
         type="text"
         placeholder="Additional Information"
-        {...register("Additional Information", {
+        name="message"
+        {...register("message", {
           required: false,
           maxLength: 250,
         })}
       />
 
-      <input className={styles.submit} type="submit" />
+      <input className={styles.submit} type="submit" value="Send" />
     </form>
   );
 }
