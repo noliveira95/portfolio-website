275d7e9d6c7d076cd0f06ebcf713cc32b8515b8d
noliveira95 <noliveira95@outlook.com>
2023-11-18 18:59:46 -0800

    refactor: remove unused imports

--- components/shared/Button/Button.jsx
+++ components/shared/Button/Button.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./Button.module.css";
-import { DownloadIcon } from "@radix-ui/react-icons";
-// import Link from "next/link";
 
 function Button({ btnText, btnIcon, onClick }) {
   return (
