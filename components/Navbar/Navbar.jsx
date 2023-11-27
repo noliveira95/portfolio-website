a14e3e854d763a833ea3798841ba7c9daf037cb0
noliveira95 <noliveira95@outlook.com>
2023-11-26 16:57:34 -0800

    refactor: start responsive navigation

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,31 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/images/logos/NickOliveLogo3Cropped.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import Button from "../shared/Button/Button";
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  const removeActive = () => {
+    setIsActive(false);
+  };
   return (
     <>
       <nav className={styles.navbar}>
-        <div className={styles["nav-links-wrapper"]}>
-          <Link href="/">
-            <Image src={logo} alt="Logo" className={styles.logo} />
-          </Link>
-          <Link className={styles["nav-link"]} href="/">
-            Who Am I
-          </Link>
-          <Link className={styles["nav-link"]} href="/">
-            My Skills
-          </Link>
-          <Link className={styles["nav-link"]} href="/">
-            My Work
-          </Link>
-        </div>
-        <div className={styles["social-icons-wrapper"]}>
+        <Link href="/">
+          <Image src={logo} alt="Logo" className={styles.logo} />
+        </Link>
+        <ul
+          className={`${styles["nav-links"]} ${isActive ? styles.active : ""}`}
+        >
+          <li className={styles["nav-link"]} onClick={removeActive}>
+            <Link href="/">Who Am I</Link>
+          </li>
+          <li className={styles["nav-link"]} onClick={removeActive}>
+            <Link href="/">My Skills</Link>
+          </li>
+          <li className={styles["nav-link"]} onClick={removeActive}>
+            <Link href="/">My Work</Link>
+          </li>
+        </ul>
+        <div className={styles["cta-wrapper"]}>
           <Link href="/">
             <FontAwesomeIcon
               icon={faLinkedin}
@@ -40,6 +54,36 @@ function Navbar() {
           </Link>
           <Button isLinkButton={true} btnText="Contact Me" btnLink="/" />
         </div>
+        <div
+          className={`${styles.hamburger} ${isActive ? styles.active : ""}`}
+          onClick={toggleActive}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        {/* <Link className={styles["nav-link"]} href="/">
+            Who Am I
+          </Link>
+          <Link className={styles["nav-link"]} href="/">
+            My Skills
+          </Link>
+          <Link className={styles["nav-link"]} href="/">
+            My Work
+          </Link> */}
+        {/* <div className={styles["cta-wrapper"]}>
+          <Link href="/">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className={styles["social-icon"]}
+            />
+          </Link>
+          <Link href="/">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className={styles["social-icon"]}
+            />
+          </Link>
+          <Button isLinkButton={true} btnText="Contact Me" btnLink="/" />
+        </div> */}
       </nav>
     </>
   );
