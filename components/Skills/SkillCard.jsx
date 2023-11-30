5979649ba310745d39a2488003a4e3a7149831c1
noliveira95 <noliveira95@outlook.com>
2023-11-30 14:58:34 -0800

    refactor: finish SkillCard component

--- components/Skills/SkillCard.jsx
+++ components/Skills/SkillCard.jsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./Skills.module.css";
 
-function SkillCard() {
-  return <div>SkillCard</div>;
+function SkillCard({
+  title,
+  description,
+  subtitle,
+  description2,
+  imageSrc,
+  imageAlt,
+}) {
+  return (
+    <div className={styles["skill-section"]}>
+      <Image src={imageSrc} alt={imageAlt} width={80} height={80} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <div className={styles["tools-list-wrapper"]}>
+        <h4 className={styles["skills-subheading"]}>{subtitle}</h4>
+        <p>{description2}</p>
+      </div>
+    </div>
+  );
 }
 
 export default SkillCard;
--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import styles from "./Skills.module.css";
 import Image from "next/image";
+import SkillCard from "./SkillCard";
 // import { useMediaQuery, useIsClient } from "@uidotdev/usehooks";
 
 function Skills() {
@@ -10,49 +11,30 @@ function Skills() {
     <section id={styles.skills}>
       <h2 className={styles["skills-title"]}>My Skills</h2>
       <div className={styles["skills-container"]}>
-        <div className={styles["skill-section"]}>
-          <Image
-            src="/images/icons/code_development_icon.png"
-            alt="Developer icon"
-            width={80}
-            height={80}
-          />
-          <h3>Front-end Development</h3>
-          <p>
-            I have 2+ years of experience in front-end web development. My
-            portfolio includes websites, web apps, and mobile applications.
-          </p>
-          <div className={styles["tools-list-wrapper"]}>
-            <h4 className={styles["skills-subheading"]}>
-              Tools that I work with:
-            </h4>
-            <p>
-              HTML/CSS, JavaScript, TypeScript, Dart, React, Next.js, Flutter,
-              Webflow
-            </p>
-          </div>
-        </div>
-        <div className={styles["skill-section"]}>
-          <Image
-            src="/images/icons/eye_icon.png"
-            alt="Developer icon"
-            width={80}
-            height={80}
-          />
-          <h3>UX/UI Design</h3>
-          <p>
-            My experience includes creating wireframes, mockups and prototypes,
-            as well as conducting user research utilizing methods such as
-            personas and user journey maps.
-          </p>
-          <div className={styles["tools-list-wrapper"]}>
-            <h4 className={styles["skills-subheading"]}>Things I design:</h4>
-            <p>
-              Websites, Web Apps, Mobile Apps, Design Systems, Information
-              Architecture, Logos
-            </p>
-          </div>
-        </div>
+        <SkillCard
+          imageSrc={"/images/icons/code_development_icon.png"}
+          imageAlt={"Developer Icon"}
+          title={"Front-end Development"}
+          description={
+            "I have 2+ years of experience in front-end web development. My portfolio includes websites, web apps, and mobile applications."
+          }
+          subtitle={"Tools that I work with:"}
+          description2={
+            "HTML/CSS, JavaScript, TypeScript, Dart, React, Next.js, Flutter, Webflow"
+          }
+        />
+        <SkillCard
+          imageSrc={"/images/icons/eye_icon.png"}
+          imageAlt={"Eye Icon"}
+          title={"UX/UI Design"}
+          description={
+            "My experience includes creating wireframes, mockups and prototypes, as well as conducting user research utilizing methods such as personas and user journey maps."
+          }
+          subtitle={"Things I design:"}
+          description2={
+            "Websites, Web Apps, Mobile Apps, Design Systems, Information Architecture, Logos"
+          }
+        />
       </div>
     </section>
   );
