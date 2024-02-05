85252896391566628bd8740a70fea0f0fba0fa84
Nick <noliveira95@outlook.com>
2024-02-04 18:58:06 -0800

    refactor: use isValid for conditions

--- components/Portfolio/Projects/Projects.jsx
+++ components/Portfolio/Projects/Projects.jsx
@@ -13,20 +13,19 @@ function Projects({
   handlePrevious,
   handleNext,
 }) {
+  const isValid =
+    data.length > 0 && currentIndex >= 0 && currentIndex < data.length;
+
   return (
     <>
       <div className={styles["project-items"]}>
         <CustomCard
-          cardImage={
-            data.length > 0 ? data[currentIndex].logo : "/Olive256x256.png"
-          }
-          cardTitle={data.length > 0 ? data[currentIndex].title : "Title"}
-          cardText={
-            data.length > 0 ? data[currentIndex].description : "Description"
-          }
-          cardCTA={data.length > 0 ? data[currentIndex].url : "#"}
+          cardImage={isValid ? data[currentIndex].logo : "/Olive256x256.png"}
+          cardTitle={isValid ? data[currentIndex].title : "Title"}
+          cardText={isValid ? data[currentIndex].description : "Description"}
+          cardCTA={isValid ? data[currentIndex].url : "#"}
           cardCTAText={
-            data.length > 0
+            isValid
               ? data[currentIndex].is_active
                 ? data[currentIndex].type === "design"
                   ? "View on Figma"
@@ -34,7 +33,7 @@ function Projects({
                 : "Coming Soon"
               : "Coming Soon"
           }
-          isActive={data.length > 0 ? data[currentIndex].is_active : true}
+          isActive={isValid ? data[currentIndex].is_active : true}
         />
         <div className={styles["project-controls"]}>
           <button className={styles.left} onClick={handlePrevious}>
