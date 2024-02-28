fbffc2876b7a5b898235da72e2f0dc41e79a8037
Nick <noliveira95@outlook.com>
2024-02-27 16:40:06 -0800

    chore: rm comment

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -50,28 +50,7 @@ function Navbar() {
               />
             </li>
           </ul>
-
           <SocialButtonRow />
-
-          {/* <div className={styles["social-icon-wrapper"]}>
-            <SocialButton
-              link="https://www.linkedin.com/in/nick-olive/"
-              icon={faLinkedin}
-              title="LinkedIn"
-              ariaLabel="LinkedIn"
-            />
-            <SocialButton
-              link="https://www.github.com/noliveira95"
-              icon={faGithub}
-              title="GitHub"
-              ariaLabel="GitHub"
-            />
-            <SocialButton
-              link="https://www.dribbble.com/nickolive"
-              icon={faDribbble}
-              title="Dribbble"
-            />
-          </div> */}
         </div>
         <div
           className={`${styles.hamburger} ${isActive ? styles.active : ""}`}
