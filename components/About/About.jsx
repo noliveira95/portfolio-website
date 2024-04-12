6b71d26c17917312edd1bbc71ebd06069084cfca
noliveira95 <noliveira95@outlook.com>
2024-04-12 14:50:01 -0700

    feat: add images for tools, fix layout
    Added ToolsRow component to About section.
    Added images for tools.
    Fixed layout for items in ToolsRow.

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -4,40 +4,44 @@ import Image from 'next/image';
 import Link from 'next/link';
 import MyPic from '../../public/images/AISelfieNoBg.png';
 import Button from '../shared/Button/Button';
+import ToolsRow from './ToolsRow';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
   return (
     <section id="who-am-i" className={styles.about}>
-      <div className={styles['about-content-container']}>
-        <h1>Who Am I?</h1>
-        <p className={styles['about-paragraph']}>
-          My name is Nicholas Oliveira. I&apos;m a front-end web developer and
-          UX/UI designer located in Las Vegas, NV. Technologies that I commonly
-          use in my work include HTML/CSS, JavaScript, TypeScript, React,
-          Webflow, and WordPress. I also utilize Figma, FigJam, and Adobe for
-          design work such as creating mockups, prototyping, and conducting user
-          research. I have experience in every stage of the{' '}
-          <Link href="https://aws.amazon.com/what-is/sdlc/">
-            software development lifecycle.
-          </Link>
-        </p>
-        <Button
-          isLinkButton={true}
-          btnText="View My Work"
-          btnLink="/#portfolio"
-        />
+      <div className={styles['about-me-content']}>
+        <div className={styles['about-content-container']}>
+          <h1>Who Am I?</h1>
+          <p className={styles['about-paragraph']}>
+            My name is Nicholas Oliveira. I&apos;m a front-end web developer and
+            UX/UI designer located in Las Vegas, NV. Technologies that I
+            commonly use in my work include HTML/CSS, JavaScript, TypeScript,
+            React, Webflow, and WordPress. I also utilize Figma, FigJam, and
+            Adobe for design work such as creating mockups, prototyping, and
+            conducting user research. I have experience in every stage of the{' '}
+            <Link href="https://aws.amazon.com/what-is/sdlc/">
+              software development lifecycle.
+            </Link>
+          </p>
+          <Button
+            isLinkButton={true}
+            btnText="View My Work"
+            btnLink="/#portfolio"
+          />
+        </div>
+        <div className={styles['about-image-container']}>
+          <Image
+            className={styles['about-image']}
+            src={MyPic}
+            alt="Picture of Nicholas"
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
-      <div className={styles['about-image-container']}>
-        <Image
-          className={styles['about-image']}
-          src={MyPic}
-          alt="Picture of Nicholas"
-          width={300}
-          height={300}
-        />
-      </div>
+      <ToolsRow />
     </section>
   );
 }
--- components/About/ToolsRow.jsx
+++ components/About/ToolsRow.jsx
@@ -1,7 +1,73 @@
+import Image from 'next/image';
 import React from 'react';
+import styles from './About.module.css';
+import htmlLogo from '../../public/images/logos/HTML.png';
+import cssLogo from '../../public/images/logos/CSS.png';
+import jsLogo from '../../public/images/logos/JS.png';
+import tsLogo from '../../public/images/logos/TS.png';
+import nodeLogo from '../../public/images/logos/Node.png';
+import reactLogo from '../../public/images/logos/React.png';
+import nextLogo from '../../public/images/logos/Next.png';
+import flutterLogo from '../../public/images/logos/Flutter.png';
+import figmaLogo from '../../public/images/logos/Figma.png';
+import adobeLogo from '../../public/images/logos/Adobe.png';
 
 function ToolsRow() {
-  return <div>ToolsRow</div>;
+  const tools = [
+    {
+      name: 'HTML',
+      image: htmlLogo,
+    },
+    {
+      name: 'CSS',
+      image: cssLogo,
+    },
+    {
+      name: 'JavaScript',
+      image: jsLogo,
+    },
+    {
+      name: 'TypeScript',
+      image: tsLogo,
+    },
+    {
+      name: 'Node.js',
+      image: nodeLogo,
+    },
+    {
+      name: 'React',
+      image: reactLogo,
+    },
+    {
+      name: 'Next.js',
+      image: nextLogo,
+    },
+    {
+      name: 'Flutter',
+      image: flutterLogo,
+    },
+    {
+      name: 'Figma',
+      image: figmaLogo,
+    },
+    {
+      name: 'Adobe',
+      image: adobeLogo,
+    },
+  ];
+  return (
+    <div className={styles.tools}>
+      {tools.map((tool, index) => (
+        <div key={index} className={styles['tool-item']}>
+          <Image
+            className={styles['tool-image']}
+            src={tool.image}
+            alt={tool.name}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ToolsRow;
