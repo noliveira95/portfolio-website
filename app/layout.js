db3ca56c6691fe5029a4f19704075d83dc8c8241
noliveira95 <noliveira95@outlook.com>
2024-04-12 10:45:55 -0700

    style: add new font, adjust font weights
    Replaced work sans with cabin. Modified font weights for h1 and h2.

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,32 @@
-import "../styles/globals.css";
-import { Work_Sans, DM_Sans } from "next/font/google";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import '../styles/globals.css';
+import { Cabin, DM_Sans } from 'next/font/google';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 config.autoAddCss = false;
 
-const workSans = Work_Sans({
-  display: "swap",
-  subsets: ["latin"],
-  variable: "--font-work-sans",
+const cabin = Cabin({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-cabin',
 });
 
 const dmSans = DM_Sans({
-  display: "swap",
-  subsets: ["latin"],
-  variable: "--font-dm-sans",
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-dm-sans',
 });
 
 export const metadata = {
-  title: "Nicholas Oliveira: Front-end Developer & UX/UI Designer",
+  title: 'Nicholas Oliveira: Front-end Developer & UX/UI Designer',
   description:
-    "Freelance front-end developer and UX/UI designer based in Las Vegas, NV",
+    'Freelance front-end developer and UX/UI designer based in Las Vegas, NV',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${workSans.variable} ${dmSans.variable}`}>
+    <html lang="en" className={`${cabin.variable} ${dmSans.variable}`}>
       <body>
         {children}
         <Analytics />
