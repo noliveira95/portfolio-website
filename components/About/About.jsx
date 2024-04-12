20eddbe00f1a9026170be900555dd618ea2a08b8
noliveira95 <noliveira95@outlook.com>
2024-04-12 12:12:05 -0700

    style: change about content layout, refactor id

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -3,43 +3,30 @@ import styles from './About.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import MyPic from '../../public/images/SelfieGrayBG.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
   return (
-    <div id="who-am-i">
-      <section id={styles.about}>
+    <section id="who-am-i" className={styles.about}>
+      <div className={styles['about-content']}>
         <h1>Who Am I?</h1>
-        <div className={styles['about-content']}>
-          <Image
-            src={MyPic}
-            alt="Picture of Nicholas"
-            width={200}
-            height={200}
-          />
-          <p className={styles['about-paragraph']}>
-            My name is Nicholas Oliveira. I&apos;m a front-end web developer and
-            UX/UI designer located in Las Vegas, NV. Technologies that I
-            commonly use in my work include HTML/CSS, JavaScript, TypeScript,
-            React, Webflow, and WordPress. I also utilize Figma, FigJam, and
-            Adobe for design work such as creating mockups, prototyping, and
-            conducting user research. I have experience in every stage of the{' '}
-            <Link href="https://aws.amazon.com/what-is/sdlc/">
-              software development lifecycle.
-            </Link>
-          </p>
-        </div>
-        <Link href="/#my-skills" className={styles.cta}>
-          See My Experience
-          <FontAwesomeIcon
-            icon={faArrowDown}
-            size="sm"
-            className={styles['cta-icon']}
-          />
-        </Link>
-      </section>
-    </div>
+        <p className={styles['about-paragraph']}>
+          My name is Nicholas Oliveira. I&apos;m a front-end web developer and
+          UX/UI designer located in Las Vegas, NV. Technologies that I commonly
+          use in my work include HTML/CSS, JavaScript, TypeScript, React,
+          Webflow, and WordPress. I also utilize Figma, FigJam, and Adobe for
+          design work such as creating mockups, prototyping, and conducting user
+          research. I have experience in every stage of the{' '}
+          <Link href="https://aws.amazon.com/what-is/sdlc/">
+            software development lifecycle.
+          </Link>
+        </p>
+      </div>
+      <div className={styles['about-image']}>
+        <Image src={MyPic} alt="Picture of Nicholas" width={200} height={200} />
+      </div>
+    </section>
   );
 }
 
