5d316bdb8b7a94286b9be07923244a6f8676fc44
noliveira95 <noliveira95@outlook.com>
2024-04-12 10:38:28 -0700

    refactor: change heading tag

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import styles from "./About.module.css";
-import Image from "next/image";
-import Link from "next/link";
-import MyPic from "../../public/images/SelfieGrayBG.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import styles from './About.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import MyPic from '../../public/images/SelfieGrayBG.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
   return (
     <div id="who-am-i">
       <section id={styles.about}>
-        <h2>Who Am I?</h2>
-        <div className={styles["about-content"]}>
+        <h1>Who Am I?</h1>
+        <div className={styles['about-content']}>
           <Image
             src={MyPic}
             alt="Picture of Nicholas"
             width={200}
             height={200}
           />
-          <p className={styles["about-paragraph"]}>
+          <p className={styles['about-paragraph']}>
             My name is Nicholas Oliveira. I&apos;m a front-end web developer and
             UX/UI designer located in Las Vegas, NV. Technologies that I
             commonly use in my work include HTML/CSS, JavaScript, TypeScript,
             React, Webflow, and WordPress. I also utilize Figma, FigJam, and
             Adobe for design work such as creating mockups, prototyping, and
-            conducting user research. I have experience in every stage of the{" "}
+            conducting user research. I have experience in every stage of the{' '}
             <Link href="https://aws.amazon.com/what-is/sdlc/">
               software development lifecycle.
             </Link>
@@ -35,7 +35,7 @@ function About() {
           <FontAwesomeIcon
             icon={faArrowDown}
             size="sm"
-            className={styles["cta-icon"]}
+            className={styles['cta-icon']}
           />
         </Link>
       </section>
