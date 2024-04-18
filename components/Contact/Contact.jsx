e41db6079d0ff77ee928129c91153b8f8500a0ce
noliveira95 <noliveira95@outlook.com>
2024-04-18 14:09:10 -0700

    chore: add target for email link

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
         <p>
           Contact me using the form below,
           <br /> or{' '}
-          <a href="mailto:contact@nickolive.com">
+          <a href="mailto:contact@nickolive.com" target="_blank">
             click here to send me an email.
           </a>
         </p>
