9786821ba535a48a6fad4e919e49c0ac496c4038
noliveira95 <noliveira95@outlook.com>
2024-04-19 08:16:15 -0700

    style: add scroll margin top to contact section

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -4,21 +4,19 @@ import ContactForm from './ContactForm';
 
 function Contact() {
   return (
-    <div id="contact">
-      <section className={styles.contact}>
-        <h2>Contact Me</h2>
-        <p>
-          Contact me using the form below,
-          <br /> or{' '}
-          <a href="mailto:contact@nickolive.com" target="_blank">
-            click here to send me an email.
-          </a>
-        </p>
-        <div className={styles['contact-form-wrapper']}>
-          <ContactForm />
-        </div>
-      </section>
-    </div>
+    <section id="contact" className={styles.contact}>
+      <h2>Contact Me</h2>
+      <p>
+        Contact me using the form below,
+        <br /> or{' '}
+        <a href="mailto:contact@nickolive.com" target="_blank">
+          click here to send me an email.
+        </a>
+      </p>
+      <div className={styles['contact-form-wrapper']}>
+        <ContactForm />
+      </div>
+    </section>
   );
 }
 
