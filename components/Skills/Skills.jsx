d92972f6fced23b819e67b17cd498e67ea6b4080
noliveira95 <noliveira95@outlook.com>
2024-06-02 12:58:34 -0700

    build: uninstall usehooks

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -1,38 +1,37 @@
-"use client";
+'use client';
 
-import React from "react";
-import styles from "./Skills.module.css";
-import SkillCard from "./SkillCard";
-// import { useMediaQuery, useIsClient } from "@uidotdev/usehooks";
+import React from 'react';
+import styles from './Skills.module.css';
+import SkillCard from './SkillCard';
 
 function Skills() {
   return (
     <div id="my-skills">
       <section id={styles.skills}>
-        <h2 className={styles["skills-heading"]}>My Skills</h2>
-        <div className={styles["skills-container"]}>
+        <h2 className={styles['skills-heading']}>My Skills</h2>
+        <div className={styles['skills-container']}>
           <SkillCard
-            imageSrc={"/images/icons/code_development_icon.png"}
-            imageAlt={"Developer Icon"}
-            title={"Front-end Development"}
+            imageSrc={'/images/icons/code_development_icon.png'}
+            imageAlt={'Developer Icon'}
+            title={'Front-end Development'}
             description={
-              "I have 2+ years of experience in front-end development. My portfolio includes websites, web apps, and mobile applications."
+              'I have 2+ years of experience in front-end development. My portfolio includes websites, web apps, and mobile applications.'
             }
-            subtitle={"Tools that I work with:"}
+            subtitle={'Tools that I work with:'}
             description2={
-              "HTML/CSS, JavaScript, TypeScript, Dart, React, Next.js, Flutter, Webflow, WordPress"
+              'HTML/CSS, JavaScript, TypeScript, Dart, React, Next.js, Flutter, Webflow, WordPress'
             }
           />
           <SkillCard
-            imageSrc={"/images/icons/eye_icon.png"}
-            imageAlt={"Eye Icon"}
-            title={"UX/UI Design"}
+            imageSrc={'/images/icons/eye_icon.png'}
+            imageAlt={'Eye Icon'}
+            title={'UX/UI Design'}
             description={
-              "From sketches and wireframes to high-fidelity designs and prototypes. I apply design thinking methodologies to create user-centered products."
+              'From sketches and wireframes to high-fidelity designs and prototypes. I apply design thinking methodologies to create user-centered products.'
             }
-            subtitle={"Things I design:"}
+            subtitle={'Things I design:'}
             description2={
-              "Websites, Web Apps, Mobile Apps, Design Systems, Information Architecture"
+              'Websites, Web Apps, Mobile Apps, Design Systems, Information Architecture'
             }
           />
         </div>
