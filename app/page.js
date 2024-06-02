c95d12358727db0f080d8247fa9c76951639264c
noliveira95 <noliveira95@outlook.com>
2024-06-02 12:36:28 -0700

    chore: rm comments

--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ import Footer from '@/components/Footer/Footer';
 import Hero from '@/components/Hero/Hero';
 import Navbar from '@/components/Navbar/Navbar';
 import Portfolio from '@/components/Portfolio/Portfolio';
-// import Skills from '@/components/Skills/Skills';
-// import Testimonials from '@/components/Testimonials/Testimonials';
 
 export default function Home() {
   return (
@@ -13,9 +11,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      {/* <Skills /> */}
       <Portfolio />
-      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
