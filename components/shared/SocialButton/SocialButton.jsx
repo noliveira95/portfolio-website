f2c51a05df0981e50a6950b15e22b3f92816944d
noliveira95 <noliveira95@outlook.com>
2024-06-02 12:54:07 -0700

    fix: refactor to fix id error

--- components/shared/SocialButton/SocialButton.jsx
+++ components/shared/SocialButton/SocialButton.jsx
@@ -1,17 +1,21 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import styles from "./SocialButton.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styles from './SocialButton.module.css';
 
 function SocialButton(props) {
-  const { link, icon, title, ariaLabel } = props;
+  const { link, icon, ariaLabel } = props;
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      title={ariaLabel}
+      aria-label={ariaLabel}
+    >
       <FontAwesomeIcon
         icon={icon}
-        className={styles["social-icon"]}
+        className={styles['social-icon']}
         size="xl"
-        title={title}
-        aria-label={ariaLabel}
       />
     </a>
   );
--- components/shared/SocialButtonRow/SocialButtonRow.jsx
+++ components/shared/SocialButtonRow/SocialButtonRow.jsx
@@ -1,31 +1,29 @@
-import React from "react";
-import SocialButton from "../SocialButton/SocialButton";
-import styles from "./SocialButtonRow.module.css";
+import React from 'react';
+import SocialButton from '../SocialButton/SocialButton';
+import styles from './SocialButtonRow.module.css';
 import {
   faGithub,
   faLinkedin,
   faDribbble,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons';
 
 function SocialButtonRow() {
   return (
-    <div className={styles["social-icon-wrapper"]}>
+    <div className={styles['social-icon-wrapper']}>
       <SocialButton
         link="https://www.linkedin.com/in/nick-olive/"
         icon={faLinkedin}
-        title="LinkedIn"
         ariaLabel="LinkedIn"
       />
       <SocialButton
         link="https://www.github.com/noliveira95"
         icon={faGithub}
-        title="GitHub"
         ariaLabel="GitHub"
       />
       <SocialButton
         link="https://www.dribbble.com/nickolive"
         icon={faDribbble}
-        title="Dribbble"
+        ariaLabel="Dribbble"
       />
     </div>
   );
